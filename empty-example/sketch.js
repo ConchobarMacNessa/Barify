293d78b64d0f9e4c0e06a9d3bb5996778502da27
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-28 21:56:52 +0100

    added initial structure

---
+++ empty-example/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(600, 750); //horizontal and vertical size of canvas
+  background('Thistle');
+}
+// 
+// function draw(x, y) {
+//   var size = 300
+//   ellipse(x, y, size, size);
+//   var counter = 10;
+//   if (counter > 10) {
+//     size = size/2;
+//     draw(x + size, random(600), size);
+//     draw(x - size, random(600), size);
+//   }
+// }
+//
+// // function draw() {
+// //   console.log('why')
+// //   var long = getRandomInt(35, 250);
+// //   var lat = getRandomInt(370, 500);
+// //   // rect(50, 50, 10, 200); //should be (position, position, 10, rand)
+// //   for (var i = 0; i < 10; i++){
+// //     rect(long, lat, 10, 189);
+// //   }
+// // }
+// //
+// function getRandomInt(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
