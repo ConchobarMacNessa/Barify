a216435ba7ff7319ad7c37188d599e60c9d93a0e
ConchobarMacNessa <alice.binghamcarr@gmail.com>
2017-05-29 15:08:58 +0100

    set time out on bars

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,29 +1,21 @@
 function setup() {
-  createCanvas(600, 750); //horizontal and vertical size of canvas
-  background('Thistle');
+  createCanvas(600, 750);
+  background('white');
 }
-// 
-// function draw(x, y) {
-//   var size = 300
-//   ellipse(x, y, size, size);
-//   var counter = 10;
-//   if (counter > 10) {
-//     size = size/2;
-//     draw(x + size, random(600), size);
-//     draw(x - size, random(600), size);
-//   }
-// }
-//
-// // function draw() {
-// //   console.log('why')
-// //   var long = getRandomInt(35, 250);
-// //   var lat = getRandomInt(370, 500);
-// //   // rect(50, 50, 10, 200); //should be (position, position, 10, rand)
-// //   for (var i = 0; i < 10; i++){
-// //     rect(long, lat, 10, 189);
-// //   }
-// // }
-// //
-// function getRandomInt(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
+
+function draw() {
+  noStroke();
+  var long = getRandomInt(35, 560);
+  var lat = getRandomInt(50, 500);
+  var length = getRandomInt(80, 200);
+
+  fill('black');
+  rect(long, lat, 10, length);
+
+  fill('white');
+  setTimeout(function() { rect(long, lat, 10, length); }, 1000);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
